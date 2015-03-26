abc3b4789581635522104b702777487fa66ce716
Steve Pomeroy <steve@staticfree.info>
2015-03-25 20:07:50 -0400

    Moves to sub package; improves packaging

---
+++ README.md
@@ -0,0 +1,15 @@
+Django Eventbrite App
+=====================
+
+This adds support for loading Eventbrite data into your Django app.
+
+License
+-------
+
+This module is licensed under the same license as Django itself. Please see
+[the license file](LICENSE) for more details.
+
+
+This module has taken some inspiration from [Caktus Consulting Group's module
+of the same name](http://bitbucket.org/copelco/django-eventbrite), but has been
+rewritten entirely due to the age of its codebase.
---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='Django Eventbrite',
+    version='0.1.0',
+    author='Steve Pomeroy',
+    author_email='steve@staticfree.info',
+    packages=['django_eventbrite'],
+    include_package_data=True,
+    exclude_package_data={
+        '': ['*.pyc'],
+        },
+    url='https://github.com/xxv/django-eventbrite',
+    license='BSD LICENSE',
+    description='An Eventbrite app for Django',
+    long_description=README,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
